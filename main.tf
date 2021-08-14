541c680919715718aa600b9e2abecef6028c59e6
Kent Rozenberg <87657441+kentrozenberg@users.noreply.github.com>
2021-08-14 12:25:50 -0700

    Update main.tf
    Trying another code

--- main.tf
+++ main.tf
@@ -21,42 +21,34 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 
-resource "aws_s3_bucket" "b" {
-  bucket = "omernyc-tf-test-bucket"
-  acl    = "private"
-  force_destroy = true
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+resource "random_pet" "sg" {}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-830c94e3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_s3_bucket_object" "object" {
-  bucket = "omernyc-tf-test-bucket"
-  key    = "file1"
-  source = "omer123_test.png"
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  #etag = filemd5("path/to/file")
-}
-
-#multiple buckets
-variable "bucket_names" {
-    type = set(string)
-    default = ["omerbucket1-test","omerbucket2-test","omerbucket5-test"]
-}
-
-resource "aws_s3_bucket" "bucket2" {
-    for_each = var.bucket_names
-    bucket = each.value
-    acl = "private"
-
-}
-
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
